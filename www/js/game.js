f8181f7f1068c221ae584f00c55764188d6e0e6a
Danny DeBelius <danny.debelius@gmail.com>
2014-01-28 11:46:26 -0500

    Started question prototype. #39.

--- www/js/game.js
+++ www/js/game.js
@@ -1,4 +1,6 @@
 var $content = null;
+var $questionPlayer = null;
+var $questionPlayerButton = null;
 
 /*
  * Render the start screen.
@@ -31,12 +33,38 @@ var renderGameOver = function() {
 }
 
 /*
+* Click handler for the question player "play" button.
+*/
+var onQuestionPlayerButtonClick = function(){
+    // _gaq.push(['_trackEvent', 'Audio', 'Played audio story', APP_CONFIG.PROJECT_NAME, 1]);
+    $questionPlayer.jPlayer('play');
+};
+
+/*
  * On browser ready.
  */
 var onDocumentReady = function() {
     $content = $('#content');
-
     renderQuestion();
+
+    $questionPlayer = $content.find('#player');
+    $questionPlayerButton = $content.find('.play');
+
+
+    $questionPlayerButton.on('click', onQuestionPlayerButtonClick);
+
+    // Set up the STORY NARRATION player.
+    $questionPlayer.jPlayer({
+        ready: function () {
+            $(this).jPlayer('setMedia', {
+                mp3: 'http://pd.npr.org/anon.npr-mp3/npr/asc/2011/07/20110726_asc_hh.mp3',
+                oga: 'http://s.npr.org/news/specials/2014/wolves/wolf-ambient-draft.ogg'
+            }).jPlayer('pause');
+        },
+        swfPath: 'js/lib',
+        supplied: 'mp3, oga',
+        loop: false
+    });
 }
 
 /*
