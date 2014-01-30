9daf58cebcef8a9a9b7cd70676c884a84c437f49
Danny DeBelius <danny.debelius@gmail.com>
2014-01-30 14:43:53 -0500

    Timer animation

--- www/js/game.js
+++ www/js/game.js
@@ -1,7 +1,13 @@
 var $content = null;
+var $answers = null;
 var $questionPlayer = null;
 var $questionPlayButton = null;
 var $questionStopButton = null;
+var $timerBg = null;
+var $timer = null;
+var angle = 0;
+var pi = Math.PI;
+var interval = 30;
 
 /*
  * Render the start screen.
@@ -11,7 +17,7 @@ var renderStart = function() {
     var html = JST.start(context);
 
     $content.html(html);
-}
+};
 
 /*
  * Render the question screen.
@@ -21,7 +27,7 @@ var renderQuestion = function() {
     var html = JST.question(context);
 
     $content.html(html);
-}
+};
 
 /*
  * Render the game over screen.
@@ -31,26 +37,63 @@ var renderGameOver = function() {
     var html = JST.gameover(context);
 
     $content.html(html);
-}
+};
 
 /*
 * Click handler for the question player "play" button.
 */
 var onQuestionPlayButtonClick = function(){
-    // _gaq.push(['_trackEvent', 'Audio', 'Played audio story', APP_CONFIG.PROJECT_NAME, 1]);
     $questionPlayer.jPlayer('play', 0);
     $content.find('.jp-play').hide();
     $content.find('.jp-stop').show();
+    runTimer();
 };
 
 /*
 * Click handler for the question player "stop" button.
 */
 var onQuestionStopButtonClick = function(){
-    // _gaq.push(['_trackEvent', 'Audio', 'Played audio story', APP_CONFIG.PROJECT_NAME, 1]);
     $questionPlayer.jPlayer('stop');
     $content.find('.jp-stop').hide();
     $content.find('.jp-play').show();
+};
+
+/*
+* You ran out of time
+*/
+var onTimerComplete = _.once(function(){
+    $content.find('.answers li:not(.correct)').addClass('fade');
+});
+
+/*
+* Draw the timer
+*/
+var drawTimer = function(){
+    var r = ( angle * pi / 180 );
+    var x = Math.sin( r ) * 25;
+    var y = Math.cos( r ) * - 25;
+    var mid = ( angle > 180 ) ? 1 : 0;
+    var anim = 'M 0 0 v -25 A 25 25 1 '
+             + mid + ' 1 '
+             +  x  + ' '
+             +  y  + ' z';
+
+    $timer.attr( 'd', anim );
+}
+
+/*
+* Animate our question timer
+*/
+var runTimer = function() {
+  if (angle < 359){
+    drawTimer();
+    setTimeout(runTimer, interval);
+    angle++;
+  } else {
+    angle = 360 * .9999;
+    drawTimer();
+    onTimerComplete();
+  };
 };
 
 /*
@@ -60,11 +103,12 @@ var onDocumentReady = function() {
     $content = $('#content');
     renderQuestion();
 
+    $answers = $content.find('.answers li');
     $questionPlayer = $content.find('#player');
     $questionPlayButton = $content.find('.jp-play');
     $questionStopButton = $content.find('.jp-stop');
-
-
+    $timerBg = $content.find('#timer-bg');
+    $timer = $content.find('#timer');
 
     $questionPlayButton.on('click', onQuestionPlayButtonClick);
     $questionStopButton.on('click', onQuestionStopButtonClick);
@@ -84,14 +128,14 @@ var onDocumentReady = function() {
         supplied: 'mp3, oga',
         loop: false
     });
-}
+};
 
 /*
  * On window fully loaded.
  */
 var onWindowLoad = function() {
     setupResponsiveChild();
-}
+};
 
 $(document).ready(onDocumentReady);
 $(window).load(onWindowLoad);
