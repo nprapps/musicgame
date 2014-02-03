3cce328642e17f981030482e6c10bd634df1059b
Jeremy Bowers <jeremyjbowers@gmail.com>
2014-02-03 16:33:09 -0500

    Hahahahahahaha how about we initialize that db?

--- admin.py
+++ admin.py
@@ -2,8 +2,9 @@
 
 from flask import Blueprint, render_template
 
+import app_config
+import models
 from render_utils import make_context
-from models import Quiz
 
 admin = Blueprint('admin', __name__)
 
@@ -12,9 +13,11 @@ def quiz_list():
     """
     List view of quizzes in the DB, sorted by insertion order.
     """
+    models.db.init(app_config.PROJECT_SLUG, user=app_config.PROJECT_SLUG)
+
     context = make_context()
 
-    context['quizzes'] = Quiz.select()
+    context['quizzes'] = models.Quiz.select()
 
     return render_template('admin/quiz_list.html', **context)
 
@@ -23,8 +26,10 @@ def quiz_detail(quiz_id):
     """
     A detail view of a single quiz.
     """
+    models.db.init(app_config.PROJECT_SLUG, user=app_config.PROJECT_SLUG)
+
     context = make_context()
 
-    context['quiz'] = Quiz.select().where(Quiz.id == int(quiz_id))
+    context['quiz'] = models.Quiz.select().where(models.Quiz.id == int(quiz_id))
 
     return render_template('admin/quiz_detail.html', **context)
