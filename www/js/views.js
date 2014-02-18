33a804b652747087d374d261f70491af957a4ef6
Christopher Groskopf <staringmonkey@gmail.com>
2014-02-18 09:41:54 -0600

    Rollback admin changes.

--- www/js/views.js
+++ www/js/views.js
@@ -285,7 +285,7 @@ var QuestionView = Backbone.View.extend({
     saveQuestion: function() {
         var properties = this.serialize();
 
-        this.model.save(properties/*, {
+        this.model.save(properties, {
             success: _.bind(function() {
                 _.each(this.choiceViews, function(choiceView) {
                     choiceView.saveChoice();
@@ -294,7 +294,7 @@ var QuestionView = Backbone.View.extend({
             error: _.bind(function() {
                 console.log('error');
             }, this)
-        }*/);
+        });
     },
 
     close: function() {
