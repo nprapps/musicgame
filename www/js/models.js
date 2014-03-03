ed43b6c2842a6103cd09e5a35ab580c82d752567
Christopher Groskopf <staringmonkey@gmail.com>
2014-03-03 11:01:36 -0500

    Fix 301 when deploying quiz. Closes #328.

--- www/js/models.js
+++ www/js/models.js
@@ -142,7 +142,7 @@ var Quiz = ChangeTrackingModel.extend({
 
     deploy: function() {
         return $.ajax({
-            'url': '/' + APP_CONFIG['PROJECT_SLUG'] + '/admin/deploy/' + this.get('slug'),
+            'url': '/' + APP_CONFIG['PROJECT_SLUG'] + '/admin/deploy/' + this.get('slug') + '/',
             'type': 'GET',
             'success': function() {
                 console.log('Quiz deployed.');
